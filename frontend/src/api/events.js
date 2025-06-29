36b7e48f9748c0edd7acadd4c8aa0e486dd9f9e6
Durgashankar Puhan <puhandurgasankarp@gmail.com>
2025-06-29 23:34:53 +0530

    Minor update

--- frontend/src/api/events.js
+++ frontend/src/api/events.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken } from "../utils/tokenManager";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_URL = process.env.REACT_APP_API_URL ;
 
 // Helper function to set auth token in headers
 const authHeader = () => {
